df10179bd8e73dbb085d82707f3751da09641090
Bruno Fernandez <brunofernandez@msn.com>
2018-05-16 16:38:17 +0200

    first R file

---
+++ OptionPrice/firstry.R
@@ -0,0 +1,22 @@
+#first try
+
+
+
+
+#### prereqs
+#install.packages("devtools")
+#library(devtools)
+#install_github("quandl/quandl-r")
+#library(Quandl)
+
+#prereq from cran:
+#install.packages("Quandl")
+#library(Quandl)
+
+
+
+Quandl.api_key("JP4tSFZNKVnortt5bWty")
+
+#example how to get data:
+data <- Quandl("NSE/OIL")
+
